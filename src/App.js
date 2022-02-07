a7892c574332919be6cdc9f5e7b0922eab50be65
joseungjin <jn5947@naver.com>
2022-02-07 23:35:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers =[
+  {
+    'id':1
+    ,'image':'https://placeimg.com/64/64/1'
+    ,'name':'조승진'
+    ,'birthday':'960807'
+    ,'gender':'남'
+    ,'job':'대학생'
+  },
+  {
+    'id':2
+    ,'image':'https://placeimg.com/64/64/2'
+    ,'name':'홍길동'
+    ,'birthday':'960807'
+    ,'gender':'남'
+    ,'job':'대학생'
+  },
+  {
+    'id':3
+    ,'image':'https://placeimg.com/64/64/3'
+    ,'name':'이순신'
+    ,'birthday':'960807'
+    ,'gender':'남'
+    ,'job':'대학생'
+  },
+]
+
 
 function App() {
-  return (
-    <div className='gray-background'>
-      <img src={logo} lat='logo' />
-      <h2>Hello React Project</h2>
-
+  return ( 
+    <div>
+      {
+        customers.map(c=>{
+          return(<Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            birthday={c.birthday}
+            name={c.name}
+            gender={c.gender}
+            job={c.job}
+          />
+          );
+        })
+      }
     </div>
   );
 }
