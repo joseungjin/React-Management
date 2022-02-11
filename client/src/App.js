59ae8ed730db8458232a56370857b59f085f6648
joseungjin <jn5947@naver.com>
2022-02-11 23:09:48 +0900

    modify

--- client/src/App.js
+++ client/src/App.js
@@ -10,33 +10,6 @@ import { TableBody } from '@mui/material';
 import { TableFooter } from '@mui/material';
 import TableContainer from '@mui/material/TableContainer';
 
-// const customers =[
-//   {
-//     'id':1
-//     ,'image':'https://placeimg.com/64/64/1'
-//     ,'name':'조승진'
-//     ,'birthday':'960807'
-//     ,'gender':'남'
-//     ,'job':'대학생'
-//   },
-//   {
-//     'id':2
-//     ,'image':'https://placeimg.com/64/64/2'
-//     ,'name':'홍길동'
-//     ,'birthday':'960807'
-//     ,'gender':'남'
-//     ,'job':'대학생'
-//   },
-//   {
-//     'id':3
-//     ,'image':'https://placeimg.com/64/64/3'
-//     ,'name':'이순신'
-//     ,'birthday':'960807'
-//     ,'gender':'남'
-//     ,'job':'대학생'
-//   },
-// ]
-
 
 function App() {
 
